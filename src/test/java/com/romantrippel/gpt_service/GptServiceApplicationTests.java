767a02dc94229229c4342c3250c18da9eeaf578a
Roman Trippel <contact@romantrippel.com>
2025-07-10 14:27:31 +0200

    init

---
+++ src/test/java/com/romantrippel/gpt_service/GptServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.romantrippel.gpt_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GptServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
